611319e100a4101331638a23b2a95384bf279f7a
Lucas Koh <lucasfkoh@gmail.com>
2026-05-09 18:31:03 +0800

    Added backend and jinja for viewGroups page

--- routes/viewGroups.py
+++ routes/viewGroups.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template # Our group chose to use blueprints because we could keep the python for each page separate allowing us to avoide merge conflicts
-from database import get_data # Imports our python from database.py to get the file
+from flask import Blueprint, render_template, request # Our group chose to use blueprints because we could keep the python for each page separate allowing us to avoide merge conflicts
+from extensions import db
 from datetime import datetime # For use in the query
 from models import Groups, Unit, GroupType, Faculty
 import sqlalchemy as sa
@@ -9,9 +9,52 @@ import sqlalchemy as sa
 view_groups_blueprint = Blueprint('view_groups', __name__)
 
 #Paths to the page in the url browser
-@view_groups_blueprint.route('/')
 @view_groups_blueprint.route('/view-groups')
 
 def view_groups():
+    # First is to get all of the filters that allow for you to search for a grouo
+
+    # Get all faculties for the dropdown
+    faculties = db.session.execute(
+        sa.select(Faculty.FacultyID, Faculty.Name)
+    ).mappings().all()
+
+    # Get all units for the dropdown
+    units = db.session.execute(
+        sa.select(Unit.UnitID, Unit.UnitName)
+    ).mappings().all()
+
+    # Get all group types for the dropdown
+    groupTypes = db.session.execute(
+        sa.select(GroupType.GroupTypeID, GroupType.Type)
+    ).mappings().all()
+
+    faculty = request.args.get('faculty')
+    unit = request.args.get('unit')
+    group_type = request.args.get('group_type')
+
+    query = sa.select(Groups.GroupID, Groups.GroupName, Groups.Description).join(Unit, Groups.UnitID == Unit.UnitID)
+
+    # Apply filters to the query based on user selection
+    if faculty:
+        query = query.where(Unit.FacultyID == faculty)
+
+    if unit:
+        query = query.where(Groups.UnitID == unit)
+
+    if group_type:
+        query = query.where(Groups.GroupTypeID == group_type)
+
+    groups = db.session.execute(query).mappings().all()
+
+
+    return render_template('viewGroups.html', 
+        faculties=faculties, 
+        units=units, 
+        groupTypes=groupTypes,
+        groups=groups
+    )
+
     
-    return render_template('viewGroups.html')
+
+
